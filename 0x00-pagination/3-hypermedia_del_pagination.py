4e9f025b99bc4748d8d1acba209fe869e23d3379
oumaima <oumaimanaanaapro@gmail.com>
2024-06-22 18:58:44 +0100

    3

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+
+import csv
+import math
+from typing import List, Dict
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+        self.__indexed_dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def indexed_dataset(self) -> Dict[int, List]:
+        """Dataset indexed by sorting position, starting at 0
+        """
+        if self.__indexed_dataset is None:
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
+            self.__indexed_dataset = {
+                i: dataset[i] for i in range(len(dataset))
+            }
+        return self.__indexed_dataset
+
+    def get_hyper_index(self, index: int = 0, page_size: int = 10) -> Dict:
+        assert 0 < index < len(self.dataset())
+        i_data = self.__indexed_dataset
+        data = []
+        rows = 0
+        current_index = index
+        while rows < page_size and current_index < len(i_data):
+            if current_index in i_data:
+                data.append(i_data[current_index])
+                rows += 1
+            current_index += 1
+        next_index = current_index if current_index < len(i_data) else None
+
+        return {
+                "index": index,
+                "data": data,
+                "page_size": page_size,
+                "next_index": next_index,
+        }
