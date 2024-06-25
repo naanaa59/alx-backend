254dd0253137f16066e6002263dddf1498105d3c
oumaima <oumaimanaanaapro@gmail.com>
2024-06-25 17:56:45 +0100

    0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""  class BasicCache that inherits from BaseCaching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """ class BasicCache that inherits from BaseCaching """
+
+    def put(self, key, item):
+        """assign self.cache_data the item value for the key key """
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        return self.cache_data.get(key)
