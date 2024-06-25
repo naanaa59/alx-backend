d8d681903c73236d51037e762ef58844af83f5c4
oumaima <oumaimanaanaapro@gmail.com>
2024-06-25 17:59:43 +0100

    None cases

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -8,10 +8,14 @@ class BasicCache(BaseCaching):
     """ class BasicCache that inherits from BaseCaching """
 
     def put(self, key, item):
+        if key is None or item is None:
+            return
         """assign self.cache_data the item value for the key key """
         self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
         """
+        if key is None:
+            return
         return self.cache_data.get(key)
