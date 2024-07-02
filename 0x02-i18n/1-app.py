ea915c4766f886acdfa897870e2c41fde28780bb
oumaima <oumaimanaanaapro@gmail.com>
2024-07-02 15:52:56 +0100

    1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Babel flask app with Config class"""
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+
+
+class Config():
+    """ Class Config defintion"""
+
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def home():
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
